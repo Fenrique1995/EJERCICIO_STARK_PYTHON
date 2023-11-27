740eb0610fc0ab407000fd3fb1bdd8425fbc3a5c
unknown <fenrique310@gmail.com>
2023-11-27 14:19:06 -0300

    correccion

--- ejercicio_stark_04.py
+++ ejercicio_stark_04.py
@@ -11,18 +11,18 @@ El menú principal debe tener las siguientes opciones
     6 - Salir
 """
 while True: 
-    menu = int(input('ingrese numero: \n1 - Imprimir la lista de nombres junto con sus iniciales\n2 - Imprimir la lista de nombres y el código del mismo\n3 - Normalizar datos\n4 - Imprimir índice de nombres \n5 - Navegar fichas \n6 - Salir\n'))
+    menu = input('ingrese numero: \n1 - Imprimir la lista de nombres junto con sus iniciales\n2 - Imprimir la lista de nombres y el código del mismo\n3 - Normalizar datos\n4 - Imprimir índice de nombres \n5 - Navegar fichas \n6 - Salir\n')
     match menu:
-        case 1:
+        case "1":
             names = obtener_nombre(lista_personajes)
             stark_imprimir_nombres_con_iniciales(names)
-        case 2:
+        case "2":
             stark_generar_codigos_heroes(lista_personajes)
-        case 3:
+        case "3":
             stark_normalizar_datos(lista_personajes)
-        case 4:
+        case "4":
             stark_imprimir_indice_nombre(lista_personajes)
-        case 5:
+        case "5":
             stark_navegar_fichas(lista_personajes)
-        case 6:
+        case "6":
             break
